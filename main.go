55049d238654af6007dec08402095ecea3481d96
Marco Valeri <marcovaleri@hotmail.it>
2023-03-19 13:40:07 +0000

    Add views logic

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
+
+type PageData struct {
+	PageTitle string
+}
 
 func main() {
 	// Static files
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static", http.StripPrefix("/static", fs))
 
+	// Template
+	tmpl := template.Must(template.ParseFiles("./views/home.html"))
+
+	// Handle
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		data := PageData{
+			PageTitle: "Dev With It",
+		}
+		tmpl.Execute(w, data)
 	})
 	http.ListenAndServe(":80", nil)
 }
